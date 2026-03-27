4367c1b40048ff24d85cf3cb2c9734e43bee3eac
Satish Sabnis <satishsabnis@gmail.com>
2026-03-27 11:35:33 +0400

    Final Maharaj Fix

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,4 @@
-// Force Deploy 1
-// import { Redirect } from 'expo-router';
+import { Redirect } from 'expo-router';
 
 export default function Index() {
   return <Redirect href="/meal-wizard" />;
