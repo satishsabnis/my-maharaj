48536c508bd8ab26f3faee01d621a6709e252ccf
Satish Sabnis <satishsabnis@gmail.com>
2026-03-27 10:28:50 +0400

    Force deploy to master

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,5 @@
-import { Redirect } from 'expo-router';
+// Force Deploy 1
+// import { Redirect } from 'expo-router';
 
 export default function Index() {
   return <Redirect href="/meal-wizard" />;
