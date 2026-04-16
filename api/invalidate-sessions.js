1283a15dff8a80731bd4b293b54bcd87054d9808
Satish Sabnis <satish.sabnis@gmail.com>
2026-04-16 13:14:51 +0400

    fix: wildcard CORS headers on invalidate-sessions

--- api/invalidate-sessions.js
+++ api/invalidate-sessions.js
@@ -1,10 +1,10 @@
 export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, x-maharaj-secret, cache-control');
+  res.setHeader('Access-Control-Allow-Methods', '*');
+  res.setHeader('Access-Control-Allow-Headers', '*');
 
   if (req.method === 'OPTIONS') {
-    return res.status(200).end();
+    return res.status(204).end();
   }
 
   res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate, max-age=0');
