93b0706bb800f0544909a5b231f0e14aaeef5456
Satish Sabnis <satish.sabnis@gmail.com>
2026-04-16 13:12:38 +0400

    fix: add cache-control to CORS allowed headers in invalidate-sessions

--- api/invalidate-sessions.js
+++ api/invalidate-sessions.js
@@ -1,7 +1,7 @@
 export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, x-maharaj-secret, cache-control');
 
   if (req.method === 'OPTIONS') {
     return res.status(200).end();
